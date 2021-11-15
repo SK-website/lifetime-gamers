7d5926391777a8ca1200cfa6c7eab1dab51913a8
SK-website <vnali@tutanota.com>
2021-11-15 11:42:46 +0200

    add ESLint, Prettier, Redux-toolkit, store, apiURL

---
+++ src/app/api/api.js
@@ -0,0 +1,9 @@
+const URL = './gamesList.json';
+
+const getData = async () => {
+  const response = await fetch(URL);
+  const result = await response.json();
+  return result;
+};
+
+export default getData();
